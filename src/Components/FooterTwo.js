7b85201e840d436ebac5a50b3c03ee62962500fa
Athithyan61 <athithyanathithyan06@gmail.com>
2025-12-24 16:57:55 +0530

    FooerTwo24

--- src/Components/FooterTwo.js
+++ src/Components/FooterTwo.js
@@ -1,59 +1,34 @@
-import React from "react";
-import styles from "../Styles/FooterTwo.module.css";
+import React from 'react'
+import styles from "../Styles/FooterTwo.module.css"
+import Footer from "../Components/Footer1.js"
 export default function FooterTwo() {
   return (
-    <footer className={styles.footer6}>
-      <div className={styles.container6}>
-        {/* LEFT SIDE */}
-        <div className={styles.left6}>
-          <div className={styles.brandRow6}>
-            <div className={styles.logoWrap6}>
-              <span className={styles.logoIcon6}>🌿</span>
-              <span className={styles.logoText6}>leaflife</span>
+    <div>
+      <div className={styles.FTDream}>
+        <div className={styles.heroCard}>
+          {/* Navigation Overlay */}
+          <nav className={styles.nav}>
+            <div className={styles.pills}>
+              <button className={styles.pill}>Home</button>
+              <button className={styles.pill}>Garden</button>
+              <button className={styles.pill}>Landscape Design</button>
+              <button className={styles.pill}>Expert</button>
             </div>
+            <div className={styles.arrowCircle}>
+              <span className={styles.arrowIcon}>↗</span>
+            </div>
+          </nav>
 
-            {/* vertical line */}
-            <span className={styles.line6}></span>
-
-            <p className={styles.brandDesc6}>
-              Stay updated with our latest projects and gardening tips.
-            </p>
-          </div>
-
-          <div className={styles.subscribe6}>
-            <input type="email" placeholder="Email" className={styles.input6} />
-            <button className={styles.button6}>Sign Up</button>
-          </div>
-        </div>
-
-        {/* RIGHT SIDE */}
-        <div className={styles.right6}>
-          <div className={styles.column6}>
-            <h4 className={styles.heading6}>LINKS</h4>
-            <ul>
-              <li>HOME</li>
-              <li>ABOUT US</li>
-              <li>SERVICES</li>
-              <li>PROJECTS</li>
-            </ul>
-          </div>
-
-          <div className={styles.column6}>
-            <h4 className={styles.heading6}>SERVICES</h4>
-            <ul>
-              <li>HARDSCAPING</li>
-              <li>PLANT SELECTION</li>
-              <li>GARDEN DESIGN</li>
-              <li>GARDEN CONSULTANT</li>
-            </ul>
-          </div>
+          {/* Headline */}
+          <h1 className={styles.headline}>
+            MAKE YOUR DREAM<br />
+            GARDEN INTO REALITY
+          </h1>
         </div>
       </div>
 
-      {/* BOTTOM */}
-      <div className={styles.bottom6}>
-        COURTESY © 2023. ALL RIGHTS RESERVED.
-      </div>
-    </footer>
-  );
+      <Footer />
+
+    </div>
+  )
 }
